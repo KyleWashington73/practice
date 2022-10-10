8da74809280bef04d3fa7955a9d8a4991dda9549
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-10-09 20:58:53 -0700

    latest push

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	* Template Name: About
+	*
+	* @package practice
+	*
+	*/
+?>
+<?php get_header(); ?>
+	<!-- code goes here -->
+<?php get_footer();?>
---
+++ homepage.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	* Template Name: Home
+	*
+	* @package practice
+	*
+	*/
+?>
+<?php get_header(); ?>
+	<!-- code goes here -->
+<?php get_footer();?>
