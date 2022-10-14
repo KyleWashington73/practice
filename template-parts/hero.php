e6d72865539c9e3e3af27fbfc22b8865dfabdfe3
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-10-13 17:08:24 -0700

    hero section

---
+++ template-parts/hero.php
@@ -0,0 +1,15 @@
+<section class="section-1">
+<div class="container">
+  <img src="http://practice.local/wp-content/uploads/2022/10/background-image-2.png-scaled.jpg" alt="Snow" style="width:100%;">
+  <div class="bottom-left"></div>
+  <div class="top-left"><h1>10 simple ways</h1> <br> <p class="word">TO MANAGE SUGAR</p> <br> <p class="words">As we go about our hectic, busy days, it <br> can be very easy to resort to a sugary <br> snack midway through the afternoon to <br> keep your energy levels elevated.</p></div>
+  <div class="top-right"></div>
+  <div class="bottom-right"></div>
+  <div class="centered"></div>
+</div>
+
+</section>
+
+<section class="section-2">
+    Section 2
+</section>
