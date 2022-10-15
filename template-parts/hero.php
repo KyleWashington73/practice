a4d416ec8cf7179637f56e5919a2ad38213870e5
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-10-14 19:17:28 -0700

    hero section

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,13 +1,15 @@
 <section class="section-1">
 <div class="container">
   <img src="http://practice.local/wp-content/uploads/2022/10/background-image-2.png-scaled.jpg" alt="Snow" style="width:100%;">
-  <div class="bottom-left"></div>
-  <div class="top-left"><h1>10 simple ways</h1> <br> <p class="word">TO MANAGE SUGAR</p> <br> <p class="words">As we go about our hectic, busy days, it <br> can be very easy to resort to a sugary <br> snack midway through the afternoon to <br> keep your energy levels elevated.</p></div>
-  <div class="top-right"></div>
-  <div class="bottom-right"></div>
-  <div class="centered"></div>
+  <div class="top-left"><h1 class="title">10 simple ways</h1> <br> <p class="word">TO MANAGE SUGAR</p> <br> <p class="words">As we go about our hectic, busy days, it <br> can be very easy to resort to a sugary <br> snack midway through the afternoon to <br> keep your energy levels elevated.</p><button class="button">read more</button></div>
+  <div class="content-flow">
+    <div class="even-columns">
+      <div class="col"><img src="http://practice.local/wp-content/uploads/2022/10/new-icon.png" alt="" class="jug-icon"><h4 class="mess">Drink apple cider vinegar </h4><p class="age">According to Sepel, adding one or two  tablespoons of apple cider venegar to a <br> large bottle of water</p></div>
+      <div class="col"><img src="http://practice.local/wp-content/uploads/2022/10/Untitled-design-5.png" class="menu-icon"><h4 class="mess">Keep to regular mealtimes </h4><p class="age">This may seem a little like an obvious suggestion, but eating your meals at regular,spaced  out intervals</p></div>
+      <div class="col"><img src="http://practice.local/wp-content/uploads/2022/10/fruit-logo.png" alt="" class="fruit-icon"><h4 class="mess">Eat fruit in the morning </h4><p class="age">We're told that we should eat five portions of fruits and vegetables a day in an effort to maintain a healthy lifestyle</p></div>
+    </div>
+  </div>
 </div>
-
 </section>
 
 <section class="section-2">
